54ecbebb822a47df4a4190d087fecf32ff7e3cb3
Rakshith Shenoy <rakshithshenoy@outlook.com>
2023-03-23 09:55:19 +0530

    add class 3 content

---
+++ class3_4/05_Constants.sol
@@ -0,0 +1,23 @@
+//Constants are variables that cannot be modified.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
+
+//Immutable variables are like constants. Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
+
+contract Immutable {
+    // coding convention to uppercase constant variables
+    address public immutable MY_ADDRESS;
+    uint public immutable MY_UINT;
+
+    constructor(uint _myUint) {
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
